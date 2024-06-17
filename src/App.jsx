234657635f3cd2e277a96ec42f64af8783db014c
Darina Jurovskaja <darina@Darinas-MacBook-Pro.local>
2024-06-17 16:35:51 +0100

    Updates App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import { useState } from "react";
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import Header from "./components/Header/Header";
-// import Article from './components/Article/Article'
-import NavBar from "./components/NavBar/NavBar";
-import Home from "./components/Home/Home";
+import { useState } from 'react'
+import './App.css'
+import { Routes, Route } from "react-router-dom"
+import Header from './components/Header/Header'
+import NavBar from './components/NavBar/NavBar'
+import Home from './components/Home'
 
 function App() {
+
   return (
-    <div className="app-container">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <NavBar />
+    <div className = "app-container">
+    <Header />
+    <Routes>
+      <Route path = "/" element = { <Home /> } />
+    </Routes>
+    <NavBar />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
