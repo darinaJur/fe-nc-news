807994fbe874df2e4790a9175c19f4d4d13e7961
Darina Jurovskaja <darina@Darinas-MacBook-Pro.local>
2024-06-21 10:41:43 +0100

    Add error handling for non-existing article page and when network is offline

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -4,46 +4,58 @@ import { getArticleById } from "../../../api";
 import "./Article.css";
 import Comments from "../Comments/Comments";
 import CommentAdder from "../CommentAdder/CommentAdder";
+import ErrorPage from "../ErrorPage/ErrorPage";
 
 const Article = () => {
   const { article_id } = useParams();
   const [article, setArticle] = useState({});
   const [isLoading, setIsLoading] = useState(true);
   const [comments, setComments] = useState([]);
+  const [articleError, setArticleError] = useState(null)
 
   useEffect(() => {
     getArticleById(article_id).then(( data ) => {
       setArticle(data);
       setIsLoading(false);
-    });
+    })
+    .catch((err) => {
+        setArticleError(err)
+    })
   }, [article_id]);
 
-  if (isLoading)
+  if (articleError) {
+    return (<ErrorPage error={articleError}/>) 
+  }
+
+  if (isLoading) {
     return (
       <div className="loading-container">
         <p>Loading, please wait</p>
         <img src="https://cdn.pixabay.com/photo/2015/10/31/12/04/bank-1015368_1280.jpg" />
       </div>
-    );
+    )
+  }
 
-  return (
-    <div className = "article-page">
-    <main className="article-container">
-      <img src={article.article_img_url} alt="" />
-      <h2>{article.title}</h2>
-      <div className="article-body">
-        <p>{article.body}</p>
+    return (
+     <div className = "article-page">
+      <main className="article-container">
+        <img src={article.article_img_url} alt="" />
+        <h2>{article.title}</h2>
+        <div className="article-body">
+          <p>{article.body}</p>
+        </div>
+        <p>article votes: {article.votes}</p>
+      </main>
+      <div>
+        <CommentAdder setComments={setComments} article_id={article_id} comments={comments}/>
       </div>
-      <p>article votes: {article.votes}</p>
-    </main>
-    <div>
-      <CommentAdder setComments={setComments} article_id={article_id} comments={comments}/>
-    </div>
-    <section className = "comments-container">
-        <Comments article_id = {article_id} comments={comments} setComments={setComments}/>
-    </section>
-    </div>
-  );
+      <section className = "comments-container">
+          <Comments article_id = {article_id} comments={comments} setComments={setComments}/>
+      </section>
+      </div>
+    )
+  
+
 };
 
 export default Article;
--- src/components/CommentAdder/CommentAdder.jsx
+++ src/components/CommentAdder/CommentAdder.jsx
@@ -4,7 +4,9 @@ import { postComment } from "../../../api";
 
 const CommentAdder = ({ setComments, article_id }) => {
   const [newComment, setNewComment] = useState("");
-  const [confirmMessage, setConfirmMessage] = useState(null)
+  const [confirmMessage, setConfirmMessage] = useState(null);
+  const [error, setError] = useState(null);
+  const [errPopup, setErrPopup] = useState(false);
 
   const handleChange = (event) => {
     event.preventDefault();
@@ -14,21 +16,35 @@ const CommentAdder = ({ setComments, article_id }) => {
   const handleSubmit = (event) => {
     event.preventDefault();
     postComment(newComment, article_id)
-    .then((newCommentFromApi) => {
+      .then((newCommentFromApi) => {
         setNewComment("");
-        setConfirmMessage('Your comment has been posted')
+        setConfirmMessage("Your comment has been posted");
         setComments((currentComments) => {
-            return [newCommentFromApi, ...currentComments];
+          return [newCommentFromApi, ...currentComments];
         });
-    })
+      })
+      .catch((err) => {
+        setError("Something went wrong");
+        setErrPopup(true);
+        setTimeout(() => {
+          setErrPopup(false);
+        }, 1500);
+      });
   };
+
   return (
     <div>
+      {error ? errPopup && <div className="popup">{error}</div> : null}
       <form className="new-comment">
-        <textarea value = {newComment} onChange = {handleChange} placeholder="Write your comment here" required />
+        <textarea
+          value={newComment}
+          onChange={handleChange}
+          placeholder="Write your comment here"
+          required
+        />
       </form>
       <button onClick={handleSubmit}>Post comment</button>
-    <p>{confirmMessage}</p>
+      <p>{confirmMessage}</p>
     </div>
   );
 };
